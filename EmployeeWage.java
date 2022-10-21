b52559baf9ee2a51cfe0b1fc00057ef21d937de5
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-10-21 18:20:15 -0500

    uc4-salary-switchcase

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,13 +4,20 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         Random random = new Random();
-        int a = random.nextInt(2);
+        int a = random.nextInt(3);
         int wagePH = 20;
         int FulldayH = 8;
-        if(a == 1)
-            System.out.println("Employee is Present\nPer day Wage is: "+(wagePH*FulldayH));
-        else
-            System.out.println("Employee is Absent\nPer day Wage is: 0");
-
+        int ParttimedayH = 4;
+        switch (a) {
+            case 0:
+                System.out.println("Employee is Absent\nPer day Wage is: 0");
+                break;
+            case 1:
+                System.out.println("Employee is Present\nPer day Wage is: " + (wagePH * FulldayH));
+                break;
+            case 2:
+                System.out.println("Employee is Present Halfday\nPer day Wage is: " + (wagePH * ParttimedayH));
+                break;
+        }
     }
 }
