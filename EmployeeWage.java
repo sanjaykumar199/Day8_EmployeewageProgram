a5bb3f947632749dd8c6016ad5c8604dae872a88
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-10-21 17:55:22 -0500

    Added: Employee present-absent logic

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,15 @@
 package Com.bridgelab.day8.Emplyeewage;
-
+import java.util.*;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation program");
+
+        Random random = new Random();
+        int a = random.nextInt(2);
+        if(a == 1)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+
     }
 }
