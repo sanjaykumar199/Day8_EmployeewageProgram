a75979bffe79812261ef8edbc3ad19b8f8c597f6
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-10-21 18:13:48 -0500

    uc3-salary-parttime

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,11 +4,14 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         Random random = new Random();
-        int a = random.nextInt(2);
+        int a = random.nextInt(3);
         int wagePH = 20;
         int FulldayH = 8;
+        int ParttimedayH = 4;
         if(a == 1)
             System.out.println("Employee is Present\nPer day Wage is: "+(wagePH*FulldayH));
+        else if(a == 2)
+            System.out.println("Employee is Present Halfday\nPer day Wage is: "+(wagePH*ParttimedayH));
         else
             System.out.println("Employee is Absent\nPer day Wage is: 0");
 
