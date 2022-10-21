5abc98d2b457d75d001dc7cd63c638bdb357f924
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-10-21 18:04:43 -0500

    uc2-present-absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,14 +2,15 @@ package Com.bridgelab.day8.Emplyeewage;
 import java.util.*;
 public class EmployeeWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation program");
-
+        System.out.println("Welcome to Employee Wage Computation Program");
         Random random = new Random();
         int a = random.nextInt(2);
+        int wagePH = 20;
+        int FulldayH = 8;
         if(a == 1)
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present\nPer day Wage is: "+(wagePH*FulldayH));
         else
-            System.out.println("Employee is Absent");
+            System.out.println("Employee is Absent\nPer day Wage is: 0");
 
     }
 }
