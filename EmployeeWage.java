d2e7f6a16e647845fd3fcfeefcd3bbd8f7fe3991
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-10-21 17:47:13 -0500

    employeewage

---
+++ EmployeeWage.java
@@ -0,0 +1,7 @@
+package Com.bridgelab.day8.Emplyeewage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation program");
+    }
+}
